41f669a4fe9a59e658752d6ffeec568c87385948
Alex <a.havell@sussex.ac.uk>
2022-05-04 15:32:22 +0100

    find ip addy of servers from windows

---
+++ criticalips.ps1
@@ -0,0 +1,8 @@
+﻿# this will take a txt file list of servers and return the IP for each one in a file called criticallist.txt 
+# remember to put the txt file location in the get-content 
+
+$servers = get-content " "
+
+foreach ($i in $servers )
+{ Resolve-DnsName $i | select -first 1 | ft Name, IPAddress -HideTableHeaders | out-file -Append \Documents\criticallist.txt
+}
